db277618f0c9c348e671ec0fdc16706fb4141acb
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-10-13 14:14:12 +0530

    Time: 740 ms (8.5%), Space: 45.9 MB (5.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,35 +1,19 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];  
-
-        for (char c : s1.toCharArray()) {
-            freq1[c - 'a']++;
-        }
-
-        int windowSize = s1.length();
-
-        for (int i = 0; i < s2.length(); i++) {
-            freq2[s2.charAt(i) - 'a']++;
-
-            if (i >= windowSize) {
-                freq2[s2.charAt(i - windowSize) - 'a']--;
-            }
-
-            if (matches(freq1, freq2)) {
+    public boolean checkInclusion(String st1, String st2) {
+        int s1 =st1.length();
+        int s2 =st2.length();
+        if(s1>s2) return false;
+        char ch[] = st1.toCharArray();
+        Arrays.sort(ch);
+        for(int i=0;i<=s2-s1;i++){
+            String sb = st2.substring(i,i+s1);
+            char ch2[] = sb.toCharArray();
+            Arrays.sort(ch2);
+            if(Arrays.equals(ch,ch2)){
                 return true;
             }
         }
 
-        return false;
-    }
-
-    private boolean matches(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) return false;
-        }
-        return true;
-    }
+            return false;
+     }
 }
