14e51014b35e6fd5933694925f58b942611ffe7b
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-12-03 12:15:59 +0530

    Time: 15 ms (18.69%), Space: 46.5 MB (12.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
         s=s.replaceAll("[^a-zA-Z0-9]","");
-        if(s.isEmpty()) return true;
+        s=s.toLowerCase();
         int i=0;
         int j=s.length()-1;
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)){
-                return false;
+            return false;
             }
             i++;
             j--;
