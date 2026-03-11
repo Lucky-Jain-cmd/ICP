fa9bf7bdcd8ad451582deb38d95a007108d9f624
Lucky Jain <luckyjain9548@gmail.com>
2026-03-11 12:35:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,26 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<n;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> hm.get(a)-hm.get(b));
-        for(int key : hm.keySet()){
-            pq.add(key);
-            if(pq.size()>k){
-                pq.poll();
+    public int[] topKFrequent(int[] arr, int k) {
+        
+
+         int n = arr.length;
+            HashMap<Integer,Integer> hm = new HashMap<>();
+            for(int i=0;i<n;i++){
+                hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
             }
-        }
-        int res[] = new int[k];
-        for(int i=0;i<k;i++){
-            res[i]=pq.poll();
-        }
-        return res;
+            List<Integer> al = new ArrayList<>(hm.keySet());
+            Collections.sort(al,(a,b)-> {
+                int fa = hm.get(a);
+                int fb = hm.get(b);
+            if(fa!=fb){
+                return fb-fa;  }
+            else {
+                return b-a; 
+        }});
+            int res[] = new int[k];
+            for(int i=0;i<k;i++){
+                res[i]=al.get(i);
+            }
+            return res;
+        
     }
 }
